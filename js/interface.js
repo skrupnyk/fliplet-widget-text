33008830051beb9ff07919cc5cceabdb5fb0223c
skrupnyk <serhii.kr@upplabs.com>
2022-03-21 13:26:14 +0200

    lint fix

--- js/interface.js
+++ js/interface.js
@@ -3,12 +3,12 @@ var widgetData = Fliplet.Widget.getData(widgetId) || {};
 
 function saveData() {
   Fliplet.Widget.save(widgetData)
-    .then(function () {
+    .then(function() {
       Fliplet.Widget.complete();
       Fliplet.Studio.emit('reload-widget-instance', widgetId);
     });
 }
 
-Fliplet.Widget.onSaveRequest(function () {
+Fliplet.Widget.onSaveRequest(function() {
   saveData();
 });
