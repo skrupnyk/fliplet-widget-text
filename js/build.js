7fc13d9415b53e78d201e7c38a52cceba2aa6037
skrupnyk <serhii.kr@upplabs.com>
2022-03-23 18:24:45 +0200

    test

--- js/build.js
+++ js/build.js
@@ -272,8 +272,7 @@
 
             ed.on('blur', function() {
               if (!$WYSIWYG_SELECTOR.text().replace(/[\r\n]+/g, '')) {
-                cleanUpContent();
-                init();
+                insertPlaceholder();
 
                 return;
               }
